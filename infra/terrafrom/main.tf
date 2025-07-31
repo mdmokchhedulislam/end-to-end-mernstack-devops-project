47815156d53388926fb3d0d8fa1882ac986ed229
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-07-31 11:02:27 +0600

    terraform code updated

--- infra/terrafrom/main.tf
+++ infra/terrafrom/main.tf
@@ -1,12 +1,32 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.6.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "my-linked-tf-test-bucket2"
+    key     = "data/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
 
+
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
 
 module "vpc" {
   source = "./modules/vpc"
 }
 
 module "eks" {
-  source = "./modules/eks"
-  vpc_subnet_ids = [module.vpc.private_subnet_id, module.vpc.public_subnet_id ]
+  source                 = "./modules/eks"
+  vpc_subnet_ids         = [module.vpc.private_subnet_id, module.vpc.public_subnet_id]
   worker_node_subnet_ids = [module.vpc.private_subnet_id]
 }
--- infra/terrafrom/modules/vpc/main.tf
+++ infra/terrafrom/modules/vpc/main.tf
@@ -12,6 +12,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.project_vpc.id
   cidr_block = var.public_subnet_cidr
   map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "${var.project_name}-public"
@@ -21,6 +22,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.project_vpc.id
   cidr_block = var.private_subnet_cidr
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "${var.project_name}-private"
