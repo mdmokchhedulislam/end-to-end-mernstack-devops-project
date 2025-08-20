ae5ecfd29b0c9bd42e7f46ddcf24cec7a7386ad6
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-08-20 15:07:35 +0600

    code path change

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -22,11 +22,11 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./modules/vpc"
+  source = "../../../terrafrom/modules/vpc"
 }
 
 module "eks" {
-  source                 = "./modules/eks"
+  source                 = "../../../terrafrom/modules/eks"
   vpc_subnet_ids         = [module.vpc.private_subnet_id, module.vpc.public_subnet_id]
   worker_node_subnet_ids = [module.vpc.private_subnet_id]
 }
