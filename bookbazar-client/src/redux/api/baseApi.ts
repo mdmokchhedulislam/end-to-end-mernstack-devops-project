b93ecb01bde50b6155bb98737c90d12b5f302b7a
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-08-20 19:41:46 +0600

    url update

--- bookbazar-client/src/redux/api/baseApi.ts
+++ bookbazar-client/src/redux/api/baseApi.ts
@@ -3,6 +3,6 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   // baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080" }),
-  baseQuery: fetchBaseQuery({ baseUrl: "http://52.206.120.212:8080" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://44.201.193.84:8080" }),
   endpoints: () => ({}),
 });
