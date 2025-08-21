20043c9d089aee41270ba52b35de1777cd0a4e1b
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-08-21 10:30:37 +0600

    code update with url and port

--- bookbazar-backend/src/server.ts
+++ bookbazar-backend/src/server.ts
@@ -5,12 +5,13 @@ import app from './app';
 
 let server: Server;
 
+
 async function main() {
   try {
 
-    await mongoose.connect(config.database_url as string);
+    await mongoose.connect(DATABASE_URL as string);
 
-    server = app.listen(config.port, () => {
+    server = app.listen(PORT, () => {
       console.log(`BookBazaar server is listening on port ${config.port}`);
     });
   } catch (err) {
