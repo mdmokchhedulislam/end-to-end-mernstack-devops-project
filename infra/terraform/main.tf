473580628917b9f92ec2d074e3577ed5f45aa9f7
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-09-04 08:54:45 +0600

    terraform code update

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -22,11 +22,14 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "../../../terraform/modules/vpc"
+  source = "./modules/vpc"
+
 }
 
 module "eks" {
-  source                 = "../../../terraform/modules/eks"
-  vpc_subnet_ids         = [module.vpc.private_subnet_id, module.vpc.public_subnet_id]
-  worker_node_subnet_ids = [module.vpc.private_subnet_id]
+  source                 = "./modules/eks"
+  public_subnet_id       = module.vpc.public_subnet_id_1
+  vpc_id                 = module.vpc.vpc_id
+  vpc_subnet_ids         = [module.vpc.private_subnet_id_1, module.vpc.private_subnet_id_2]
+  worker_node_subnet_ids = [module.vpc.private_subnet_id_1, module.vpc.private_subnet_id_2]
 }
